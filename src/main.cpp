4884fba5e236d5bcc2ffc62277d6eabcfc954fc5
trotFunky <teo.couprie_diaz@telecom-sudparis.eu>
2017-11-11 07:24:20 +0100

    Premier push 1A de la tristitude 1A 0 - 1 AX12
    (+Un éventuel vrai truc dans le OrderManager si j'ai bien comprit comment ça marchait)

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,19 @@
 
 #include "Com/OrderManager.h"
 
+HardwareSerial God;
+DynamixelInterface Jesus(God,1);
+
+DynamixelPacket* evangileSelonStPaul;
+DynamixelPacket* evangileSelonStPierre;
+DynamixelPacket* evangileSelonStMarc;
+
+uint8_t go0[2] = { 0x00 , 0x00 };
+uint8_t go300[2] = { 0xFF , 0x03 };
+uint8_t speed[2] = { 42 , 0};
+
+DynamixelMotor LaBible(Jesus,254);
+
 //Initialisation de la Serie
 void setup() {
 	/*serie*/
@@ -14,21 +27,41 @@ void setup() {
 	Serial.println("Série OK");
 	delay(250);
 
-	/*Wire.begin();
-	for (int i = 0; i < US_TOTAL; i++) {
-		boolean error = true;
-		while (error) {
-			Wire.beginTransmission(SLAVE_ADDRESS);
-			error = Wire.endTransmission(I2C_STOP, 50000); // if error !=0, connection error(50ms timeout)
-			if (error) { // Not connected, try to reset bus and connect again
-				Serial.println("\nNo I2C connection\n...trying again\n");
-				Wire.resetBus();
-				Wire.begin();
-				Wire.setClock(400000);
-			}
-			delay(200);
-		}
-	}*/
+	God.begin(115200);
+
+	LaBible.init();
+	LaBible.enableTorque();
+	LaBible.jointMode();
+	LaBible.speed(42);
+
+    //evangileSelonStPaul = new DynamixelPacket(0xFE,0x03,5,go0,0x1E);
+    //evangileSelonStPierre = new DynamixelPacket(0xFE,0x03,5,go300,0x1E);
+	//evangileSelonStMarc = new DynamixelPacket(0xFE,0x03,5,speed,0x20);
+
+	//Serial.println("On a écrit les évangiles");
+	//delay(1000);
+
+	//Jesus->sendPacket(*evangileSelonStMarc);
+
+	//Serial.println("Jesus donne le tempo");
+	//delay(1000);
+
+
+    /*Wire.begin();
+    for (int i = 0; i < US_TOTAL; i++) {
+        boolean error = true;
+        while (error) {
+            Wire.beginTransmission(SLAVE_ADDRESS);
+            error = Wire.endTransmission(I2C_STOP, 50000); // if error !=0, connection error(50ms timeout)
+            if (error) { // Not connected, try to reset bus and connect again
+                Serial.println("\nNo I2C connection\n...trying again\n");
+                Wire.resetBus();
+                Wire.begin();
+                Wire.setClock(400000);
+            }
+            delay(200);
+        }
+    }*/
 }
 
 /* Interruptions d'asservissements */
@@ -41,23 +74,37 @@ void motionControlInterrupt() {
 
 //Boucle principale, gere entre autres la communication avec le HL
 void loop() {
-	OrderManager& orderMgr = OrderManager::Instance();
+    //Broadcast ID : 0xFE
+    //Jesus->sendPacket(*evangileSelonStPaul);
+	//Serial.println("St. Paul a parlé");
+    //delay(1000);
+    //Jesus->sendPacket(*evangileSelonStPierre);
+	//Serial.println("St Pierre a parlé");
+    //delay(1000);
 
-	/* MotionControlSystem */
-	IntervalTimer motionControlInterruptTimer;
-	motionControlInterruptTimer.priority(253);
-	motionControlInterruptTimer.begin(motionControlInterrupt, MC_PERIOD); //asservissements
-	
-	/* Gestion des ordres recus */
-
-	while (true) {
-		//orderMgr.refreshUS();
-		orderMgr.communicate();
-		//orderMgr.sendUSData();
-	}
+	delay(1000);
+	LaBible.goalPositionDegree(0);
+	delay(1000);
+	LaBible.goalPositionDegree(300);
 }
 
+//Hijack du main parce que fuck platformio
+void loop2(){
+    OrderManager& orderMgr = OrderManager::Instance();
 
+    /* MotionControlSystem */
+    IntervalTimer motionControlInterruptTimer;
+    motionControlInterruptTimer.priority(253);
+    motionControlInterruptTimer.begin(motionControlInterrupt, MC_PERIOD); //asservissements
+
+    /* Gestion des ordres recus */
+
+    while (true) {
+        //orderMgr.refreshUS();
+        orderMgr.communicate();
+        //orderMgr.sendUSData();
+    }
+}
 
 
 
