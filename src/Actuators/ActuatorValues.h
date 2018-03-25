69162660f461f26e939d235af0a5721b17e5c397
asphox <aspirine026@gmail.com>
2018-03-25 05:05:40 +0200

    petite modif angle porte un peu

--- src/Actuators/ActuatorValues.h
+++ src/Actuators/ActuatorValues.h
@@ -44,7 +44,7 @@ constexpr int backFolded = 120;
  */
 
 constexpr int doorBaseSpeed = 300;
-constexpr int doorSmallOpeningAngle = 15;
+constexpr int doorSmallOpeningAngle = 10;
 
 constexpr int frontDoorOpen = 240;
 constexpr int frontDoorClosed = 155;
